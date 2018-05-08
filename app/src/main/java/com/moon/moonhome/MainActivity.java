7281c182b9d8df21833028a055d4f946f9222e99
luxin <luxin@hejun.com>
2018-05-08 18:56:04 +0800

    rxjava

--- app/src/main/java/com/moon/moonhome/MainActivity.java
+++ app/src/main/java/com/moon/moonhome/MainActivity.java
@@ -9,11 +9,45 @@ import com.moon.msupport.util.parse.JsonUtil;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
 
+        Observer observer = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        Observable<Object> observable = Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
+
+            }
+        });
+    }
 }

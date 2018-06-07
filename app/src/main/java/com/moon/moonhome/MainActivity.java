8fecc043774d6b7476aa272e04295c8b59a170e1
luxin <luxin@hejun.com>
2018-06-07 16:02:55 +0800

    gradle 4.4

--- app/src/main/java/com/moon/moonhome/MainActivity.java
+++ app/src/main/java/com/moon/moonhome/MainActivity.java
@@ -1,53 +1,59 @@
 package com.moon.moonhome;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import com.google.gson.reflect.TypeToken;
-import com.lzy.okgo.OkGo;
-import com.moon.msupport.util.parse.JsonUtil;
+import com.yanzhenjie.permission.Action;
+import com.yanzhenjie.permission.AndPermission;
 
 import java.util.List;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Observer observer = new Observer() {
-            @Override
-            public void onSubscribe(Disposable d) {
-
-            }
-
-            @Override
-            public void onNext(Object o) {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        };
-
-        Observable<Object> observable = Observable.create(new ObservableOnSubscribe<Object>() {
-            @Override
-            public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
-
-            }
-        });
+        AndPermission
+                .with(this)
+                .runtime()
+                .permission(Manifest.permission.CAMERA)
+                .onGranted(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> data) {
+                        Log.d(TAG, "onGranted");
+                    }
+                })
+                .onDenied(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> data) {
+                        Log.d(TAG, "onDenied");
+                        if (AndPermission.hasAlwaysDeniedPermission(MainActivity.this, Manifest.permission.CAMERA)) {
+                            AndPermission
+                                    .with(MainActivity.this)
+                                    .runtime()
+                                    .permission(Manifest.permission.CAMERA)
+                                    .rationale(new RuntimeRationale())
+                                    .onGranted(new Action<List<String>>() {
+                                        @Override
+                                        public void onAction(List<String> data) {
+                                            Log.d(TAG, "onGranted111");
+                                        }
+                                    })
+                                    .onDenied(new Action<List<String>>() {
+                                        @Override
+                                        public void onAction(List<String> data) {
+                                            Log.d(TAG, "onDenied111");
+                                        }
+                                    })
+                                    .start();
+                        }
+                    }
+                })
+                .start();
     }
 }
